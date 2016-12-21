2089d6cb9b6af065e02ee2b7c90a2b882bea5964
Jan Seeger <jan@alphadev.net>
2016-12-21 21:24:58 +0100

    cleanup.

--- src/main/java/de/lanrena/jeopardy/Application.java
+++ src/main/java/de/lanrena/jeopardy/Application.java
@@ -1,22 +1,13 @@
 package de.lanrena.jeopardy;
 
-import de.lanrena.jeopardy.model.GlobalGameState;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    @SuppressWarnings("unused")
-    public GlobalGameState getGlobalGameState() {
-        return new GlobalGameState();
     }
 }
