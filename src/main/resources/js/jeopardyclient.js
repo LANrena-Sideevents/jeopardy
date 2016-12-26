9a62d348fa6dbee2433906838fdced9c923836bd
Jan Seeger <jan@alphadev.net>
2016-12-26 03:08:11 +0100

    client js code cleanup.

--- src/main/resources/js/jeopardyclient.js
+++ src/main/resources/js/jeopardyclient.js
@@ -9,36 +9,28 @@ const handleGameAction = function() {
 
 };
 
-const subscribeToGame = function (game_id) {
-    stompClient.unsubscribe('/topic/games');
-    stompClient.subscribe('/topic/game/' + game_id, function () {
-        handleGameAction();
-    });
-
-    document.getElementById('gamelist').style.visibility = "hidden";
-    document.getElementById('playerlist').style.visibility = "visible";
-};
-
-const handleGameAdded = function (game_info) {
-    for (let game of JSON.parse(game_info))
-    {
-        let node = document.createElement("li");
-        node.innerText = game.id;
-        node.onclick = function() {
-            subscribeToGame.apply(null, [game.id]);
-        };
-
-        document.getElementById('gamelist').appendChild(node);
-    }
-};
-
 document.addEventListener("DOMContentLoaded", function() {
     const socket = new SockJS('http://[::1]:8080/jeopardy');
     stompClient = Stomp.over(socket);
 
     stompClient.connect({}, function () {
         stompClient.subscribe('/topic/games', function (game_result) {
-            handleGameAdded(JSON.parse(game_result.body).payload)
+            let game_info = JSON.parse(game_result.body).payload;
+            for (let game of JSON.parse(game_info))
+            {
+                let node = document.createElement("li");
+                node.innerText = game.id;
+                node.onclick = function() {
+                    stompClient.unsubscribe('/topic/games');
+                    stompClient.subscribe('/topic/game/' + game.id, function () {
+                        handleGameAction();
+                    });
+                    document.getElementById('gamelist').style.visibility = "hidden";
+                    document.getElementById('playerlist').style.visibility = "visible";
+                };
+
+                document.getElementById('gamelist').appendChild(node);
+            }
         });
 
         stompClient.subscribe('/topic/messages', function (msg) {
