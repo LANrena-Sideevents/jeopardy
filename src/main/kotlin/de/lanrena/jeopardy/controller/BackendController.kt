9e45d0a5917b42a471d8af6d3a9d27b08310bb1c
Jan Seeger <jan@alphadev.net>
2017-02-05 15:13:39 +0100

    * add bootstrap lib to backend. * add game loading view to backend.

--- src/main/kotlin/de/lanrena/jeopardy/controller/BackendController.kt
+++ src/main/kotlin/de/lanrena/jeopardy/controller/BackendController.kt
@@ -31,12 +31,19 @@ open class BackendController(
         return "backend/game"
     }
 
+    @GetMapping("/game/{gameid}/players")
+    fun list_players(@PathVariable("gameid") gameId: UUID?, model: Model): String {
+        val findGame = gameState.findGame(gameId!!) ?: return "redirect:/backend/index"
+        model.addAttribute("game", findGame)
+        return "backend/players"
+    }
+
     @PostMapping("/game/{gameid}/player")
     fun add_player(@PathVariable("gameid") gameId: UUID,
                    @RequestParam("player_name") player_name: String,
                    @RequestParam("player_color") player_color: String): String {
         gameState.addPlayer(gameId, player_name, player_color)
-        return "redirect:/backend/game/" + gameId
+        return "redirect:/backend/game/$gameId/players"
     }
 
     @PostMapping("game")
