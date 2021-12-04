319943d8579bcffd06a4bde692cb72c00979e54b
Jan Seeger <jan@alphadev.net>
2021-12-04 16:20:09 +0100

    Replace deprecated code

--- src/main/kotlin/de/lanrena/jeopardy/app/WebConfig.kt
+++ src/main/kotlin/de/lanrena/jeopardy/app/WebConfig.kt
@@ -3,11 +3,11 @@ package de.lanrena.jeopardy.app
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableWebMvc
 @Configuration
-open class WebConfig : WebMvcConfigurerAdapter() {
+open class WebConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/js/**")
                 .addResourceLocations("classpath:/js/")
--- src/main/kotlin/de/lanrena/jeopardy/app/WebSocketConfig.kt
+++ src/main/kotlin/de/lanrena/jeopardy/app/WebSocketConfig.kt
@@ -2,13 +2,13 @@ package de.lanrena.jeopardy.app
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
-open class WebSocketConfig : AbstractWebSocketMessageBrokerConfigurer() {
+open class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         config.enableSimpleBroker("/topic")
